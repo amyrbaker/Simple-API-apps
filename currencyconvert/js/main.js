480815c56c67e02eb0c816be828e4c77f3c29e00
Amy Baker <amyraebaker@gmail.com>
2022-04-19 21:55:32 -0700

    Add local storage

--- currencyconvert/js/main.js
+++ currencyconvert/js/main.js
@@ -1,27 +1,26 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-// document.querySelector('h2').innerHTML = localStorage.getItem('books')
+document.querySelector('h2').innerHTML = localStorage.getItem('currency')
 
 function getFetch(){
   const amount = document.querySelector('#amount').value
   const choice1 = document.querySelector('#orig').value
   const choice2 = document.querySelector('#new').value
   const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${choice1}/${choice2}.json`
-  let description = `${amount} ${choice1.toUpperCase()} is equal to ${(data[choice2] * amount).toFixed(2)} ${choice2.toUpperCase()}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        let description = `${amount} ${choice1.toUpperCase()} is equal to ${(data[choice2] * amount).toFixed(2)} ${choice2.toUpperCase()}`
+
         if(!localStorage.getItem('currency')) {
           localStorage.setItem('currency', description)
         } else {
-          let currency = localStorage.getItem('currency') + '\n' + description
+          let currency = localStorage.getItem('currency') + ', ' + description
           localStorage.setItem('currency', currency)
         }
-        // document.querySelector('h2').innerHTML = localStorage.getItem('books')
-        
-        document.querySelector('h2').innerHTML = description
+        document.querySelector('h2').innerHTML = localStorage.getItem('currency')
       })
       .catch(err => {
           console.log(`error ${err}`)
