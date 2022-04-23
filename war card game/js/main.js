6da82d242351ceb18b0d2fae5f29b509109455bf
Amy Baker <amyraebaker@gmail.com>
2022-04-23 00:23:41 -0700

    Update game to save scores to local storage

--- war card game/js/main.js
+++ war card game/js/main.js
@@ -11,6 +11,13 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 
 document.querySelector('button').addEventListener('click', drawTwo)
 
+let player1Points = 0
+let player2Points = 0
+let ties = 0
+localStorage.setItem('player1', player1Points)
+localStorage.setItem('player2', player2Points)
+localStorage.setItem('ties', ties)
+
 function drawTwo(){
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
   fetch(url)
@@ -37,14 +44,26 @@ function drawTwo(){
         }
         let player1Val = cardsArr[data.cards[0].value]
         let player2Val = cardsArr[data.cards[1].value]
+        
 
         if (player1Val > player2Val) {
             document.querySelector('h3').innerHTML = 'Player 1 Wins'
+            player1Points += 1
+            localStorage.setItem('player1', player1Points)
         } else if (player1Val < player2Val) {
             document.querySelector('h3').innerHTML = 'Player 2 Wins'
+            player2Points += 1
+            localStorage.setItem('player2', player2Points)
         } else {
             document.querySelector('h3').innerHTML = 'Time for War'
+            ties += 1
+            localStorage.setItem('ties', ties)
         }
+        document.querySelector('h4').innerHTML = `Player 1 : ${localStorage.getItem('player1')}<br/>Player 2 : ${localStorage.getItem('player2')}<br/>Ties : ${ties}`
+        if (player1Points + player2Points + ties === 26) {
+            let winner = player1Points > player2Points ? 'Player 1' : player1Points < player2Points ? 'Player 2' : 'Tie'
+            document.querySelector('h5').innerHTML = 'Game Over' + winner === 'Tie' ? 'You tie!' : `${winner} wins!`
+        } 
     })
     .catch(err => {
         console.log(`error ${err}`)
